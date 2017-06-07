d3591000d1704145fe19babfd1e31ca98ff934a9
nalhoussari <nalhoussari@hotmail.com>
2017-06-06 18:32:27 -0700

    Submitting assignement AutolayoutStoryboard

---
+++ AutoLayoutStoryboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutStoryboard
+//
+//  Created by Noor Alhoussari on 2017-06-06.
+//  Copyright © 2017 Noor Alhoussari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
